13fd1fc05da1793059b18b682d591ab33d7426c5
Kunal <kdshah6593@gmail.com>
2021-01-14 14:34:09 -0500

    commit tree 1e2644674d4f580d850fa444b06d3d21a883179b

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :students, only: [:create, :new, :show, :edit, :update]
+  
+  resources :school_classes, only: [:create, :new, :show, :edit, :update]
+
 end
